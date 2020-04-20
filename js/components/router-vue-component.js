085e1eec2908bcada8fdf7477fb90428d1379d24
Emmanuel Valentin <ev.externo@hito.com.mx>
2020-04-20 17:26:36 -0500

    se agrego font awesome y nueva seccion adjudicacion

--- js/components/router-vue-component.js
+++ js/components/router-vue-component.js
@@ -11,51 +11,51 @@ let homeComponent = {
   data: function () {
     return {
       row1KPI: [
-        { key: 0, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'qamd', title: 'Créditos',style:'kpi-xs',topBar:true, download: true, titleTop:'Créditos',
+        { key: 0, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'qamd', title: 'Créditos',style:'kpi-xs', download: true,
           kpiIn:{
             row1KPIN:[
-                      { key: 0, id: 'homeKPIN',icon:'attach_money',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false, titleTop:'Top'}
+                      { key: 0, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false}
                     ]
           , row2KPIN:[
-                      { key: 1, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false, titleTop:''}
-                    , { key: 2, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false, titleTop:''}
+                      { key: 1, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false}
+                    , { key: 2, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false}
 
                     ]
             }
         }
-      , { key: 1, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'JcJvj', title: 'Capital',style:'kpi-xs',topBar:true, download: true, titleTop:'',
+      , { key: 1, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'JcJvj', title: 'Capital',style:'kpi-xs', download: true,
             kpiIn:{
               row1KPIN:[
-                        { key: 3, id: 'homeKPIN',icon:'attach_money',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false, titleTop:'Top'}
+                        { key: 3, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false}
                       ]
             , row2KPIN:[
-                        { key: 4, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false, titleTop:''}
-                      , { key: 5, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false, titleTop:''}
+                        { key: 4, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false}
+                      , { key: 5, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false}
   
                       ]
             }
         }
-      , { key: 2, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'WJYuPN', title: 'Interés',style:'kpi-xs',topBar:true, download: true, titleTop:'',
+      , { key: 2, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'WJYuPN', title: 'Interés',style:'kpi-xs', download: true,
           kpiIn:{
             row1KPIN:[
-                      { key: 6, id: 'homeKPIN',icon:'attach_money',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false, titleTop:'Top'}
+                      { key: 6, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false}
                     ]
           , row2KPIN:[
-                      { key: 7, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false, titleTop:''}
-                    , { key: 8, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false, titleTop:''}
+                      { key: 7, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false}
+                    , { key: 8, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false}
 
                     ]
           }
         }
-      , { key: 3, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'WJYuPN', title: 'Total',style:'kpi-xs',topBar:true, download: true, titleTop:'',
+      , { key: 3, id: 'homeKPI',size:'s6 m4 l3',keyApp:0,qlik:'WJYuPN', title: 'Total',style:'kpi-xs', download: true,
           kpiIn:{
             row1KPIN:[
-                      { key: 9, id: 'homeKPIN',icon:'attach_money',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false, titleTop:'Top'}
+                      { key: 9, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s12 m12 l12',keyApp:0,qlik:'NrHfp', title: '',style:'kpiin-xs', download: false}
                    
                     ]
           , row2KPIN:[
-                      { key: 10, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false, titleTop:''}
-                    , { key: 11, id: 'homeKPIN',icon:'attach_money',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false, titleTop:''}
+                      { key: 10, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Periodo Anterior',style:'kpiin-xs', download: false}
+                    , { key: 11, id: 'homeKPIN',icon:'fas fa-dollar-sign i-tiny',size:'s6 m6 l6',keyApp:0,qlik:'ajMAEu', title: 'Año Anterior',style:'kpiin-xs', download: false}
 
                     ]
 
@@ -63,11 +63,11 @@ let homeComponent = {
         }
     ]
     ,row2KPI: [
-      { key: 4, id: 'homeKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-home' , download: false, titleTop:''}
+      { key: 4, id: 'homeKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-home' , download: false}
     ]
     ,row3KPI: [
-      { key: 5, id: 'homeKPI',size:'s12 m6 l6', keyApp:0,qlik:'vCNaSe', title: 'Gráfica ejemplo 5',style:'kpi-m',topBar:true, download: true, titleTop:''}
-    , { key: 6, id: 'homeKPI',size:'s12 m6 l6', keyApp:0,qlik:'akDGX', title: 'Gráfica ejemplo 6', style:'kpi-m',topBar:true, download: true, titleTop:''}
+      { key: 5, id: 'homeKPI',size:'s12 m6 l6', keyApp:0,qlik:'vCNaSe', title: 'Gráfica ejemplo 5',style:'kpi-m', download: true}
+    , { key: 6, id: 'homeKPI',size:'s12 m6 l6', keyApp:0,qlik:'akDGX', title: 'Gráfica ejemplo 6', style:'kpi-m', download: true}
     ]
     }
   }
@@ -78,21 +78,27 @@ let flujosComponent = {
   data: function (){
     return{
       row1KPI: [
-        { key: 0, id: 'flujosKPI',size:'s12 m8 l8',keyApp:1,qlik:'SqDbku', title: 'Gráfica ejemplo 1',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
-      , { key: 1, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 0, id: 'flujosKPI',size:'s12 m8 l8',keyApp:1,qlik:'SqDbku', title: 'Gráfica ejemplo 1',style:'kpi-m' , download: true}
+      , { key: 1, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' , download: true}
       ]
       ,row2KPI: [
-        { key: 2, id: 'flujosKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
-      , { key: 3, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 4',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 2, id: 'flujosKPI',size:'s12 m8 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-m' , download: true}
+      , { key: 3, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 4',style:'kpi-m' , download: true}
       ]
       ,row3KPI: [
-        { key: 4, id: 'flujosKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 5',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
-      , { key: 5, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 6',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 4, id: 'flujosKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 5',style:'kpi-m' , download: true}
+      , { key: 5, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 6',style:'kpi-m' , download: true}
       ]
       ,row4KPI: [
-        { key: 6, id: 'flujosKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 7',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
-      , { key: 7, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 8',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 6, id: 'flujosKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 7',style:'kpi-m' , download: true}
+      , { key: 7, id: 'flujosKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 8',style:'kpi-m' , download: true}
+      ,
       ]
+      ,row5KPI: [
+        { key: 8, id: 'flujosKPI',size:'s12 m8 l7',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 9',style:'kpi-xs' , download: true}
+      , { key: 9, id: 'flujosKPI',size:'s12 m4 l5',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 10',style:'kpi-xs' , download: true}
+      ]
+      
     }
   }
 };
@@ -102,31 +108,46 @@ let incumplimientoComponent = {
   data: function (){
     return{
       row1KPI: [
-        { key: 0, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-l' ,topBar:true, download: true, titleTop:''}
+        { key: 0, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-l' , download: true}
       ]
       ,row2KPI: [
-        { key: 1, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-l' ,topBar:true, download: true, titleTop:''}
+        { key: 1, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-l' , download: true}
       ]
       , row3KPI: [
-        { key: 2, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-l' ,topBar:true, download: true, titleTop:''}
+        { key: 2, id: 'incumplimientoKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-l' , download: true}
       ]
 
     }
   }
 };
 
+let adjudicacionComponent = {
+  template: '#adjudicacion-component',
+  data: function (){
+    return{
+      row1KPI: [
+        { key: 1, id: 'adjudicacionKPI',size:'s12 m8 l8',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' , download: true}
+      , { key: 2, id: 'adjudicacionKPI',size:'s12 m4 l4',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' , download: true}
+      ]
+      ,row2KPI: [
+        { key: 0, id: 'adjudicacionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-m' , download: true}
+      ]
+
+    }
+  }
+};
 let distribucionComponent = {
   template: '#distribucion-component',
   data: function (){
     return{
       row1KPI: [
-        { key: 0, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 0, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-m' , download: true}
       ]
       ,row2KPI: [
-        { key: 1, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 1, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-m' , download: true}
       ]
       , row3KPI: [
-        { key: 2, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-m' ,topBar:true, download: true, titleTop:''}
+        { key: 2, id: 'distribucionKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-m' , download: true}
       ]
 
     }
@@ -138,7 +159,7 @@ let reporteComponent = {
   data: function (){
     return{
       row1KPI: [
-        { key: 0, id: 'reporteKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-xl' , download: false, titleTop:''}
+        { key: 0, id: 'reporteKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-xl' , download: false}
       ]
     }
   }
@@ -150,18 +171,18 @@ let matrizComponent = {
     return{
       
       row1KPI: [
-        { key: 0, id: 'matrizKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-l',topBar:true, download: true, titleTop:''}
+        { key: 0, id: 'matrizKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 1',style:'kpi-l', download: true}
       ]
       ,row2KPI: [
-        { key: 1, id: 'matrizKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-l' ,topBar:true, download: true, titleTop:''}
+        { key: 1, id: 'matrizKPI',size:'s12 m12 l12',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 2',style:'kpi-l' , download: true}
       ]
       ,row3KPI: [
-        { key: 2, id: 'matrizKPI',size:'s2 m2 l2',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-matriz' , download: false, titleTop:''}
-      , { key: 3, id: 'matrizKPI',size:'s2 m2 l2',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-matriz' , download: false, titleTop:''}
+        { key: 2, id: 'matrizKPI',size:'s2 m2 l2',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-matriz' , download: false}
+      , { key: 3, id: 'matrizKPI',size:'s2 m2 l2',keyApp:0,qlik:'Btbd', title: '',style:'kpi-xs-matriz' , download: false}
       ]
       ,row4KPI: [
-        { key: 4, id: 'matrizKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-s' ,topBar:true, download: true, titleTop:''}
-      , { key: 5, id: 'matrizKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 4',style:'kpi-s' ,topBar:true, download: true, titleTop:''}
+        { key: 4, id: 'matrizKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 3',style:'kpi-s' , download: true}
+      , { key: 5, id: 'matrizKPI',size:'s12 m6 l6',keyApp:0,qlik:'Btbd', title: 'Gráfica ejemplo 4',style:'kpi-s' , download: true}
       ]
     }
   }
@@ -180,6 +201,7 @@ const routes = [
   { path: '/', component: homeComponent }
   ,{ path: '/Flujos', component: flujosComponent }
   ,{ path: '/Incumplimiento', component: incumplimientoComponent }
+  ,{ path: '/Adjudicacion', component: adjudicacionComponent }
   ,{ path: '/Distribucion', component: distribucionComponent }
   ,{ path: '/Reporte', component: reporteComponent }
   ,{ path: '/Matriz', component: matrizComponent }
